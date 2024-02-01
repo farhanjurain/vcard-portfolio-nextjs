8755c0561ee9e2eecaba25b8a96c9a6865abf863
soren <afrozaman12@gmail.com>
2024-02-01 19:41:57 +0600

    hardcoded the metadata

--- app/layout.js
+++ app/layout.js
@@ -1,21 +1,47 @@
 import "./globals.css";
 import SideBar from "./layouts/SideBar";
 import NavBar from "./layouts/NavBar";
-import Head from "next/head";
+import ogImage from "@/public/og-img.png"
+
+// export const metadata = {
+//   title: "Soren D. Blank",
+//   description: "Im gonna be the king of the pirates.",
+//   image: "https://next-vcard-portfolio.vercel.app/embed.png",
+//   keywords: ["Soren", "Blank", "Soren D. Blank", "Soren Blank"],
+//   "og:title" : "Soren D. Blank",
+//   "og:description" : "Im gonna be the king of the pirates.",
+//   "og:image" : "https://next-vcard-portfolio.vercel.app/embed.png",
+
+//   "twitter:title" : "Soren D. Blank",
+//   "twitter:description" : "Im gonna be the king of the pirates.",
+//   "twitter:image" : "https://next-vcard-portfolio.vercel.app/embed.png",
+// };
 
 export const metadata = {
   title: "Soren D. Blank",
   description: "Im gonna be the king of the pirates.",
   image: "https://next-vcard-portfolio.vercel.app/embed.png",
   keywords: ["Soren", "Blank", "Soren D. Blank", "Soren Blank"],
-  // "og:title" : "Soren D. Blank",
-  // "og:description" : "Im gonna be the king of the pirates.",
-  // "og:image" : "https://next-vcard-portfolio.vercel.app/embed.png",
+  openGraph: {
 
-  // "twitter:title" : "Soren D. Blank",
-  // "twitter:description" : "Im gonna be the king of the pirates.",
-  // "twitter:image" : "https://next-vcard-portfolio.vercel.app/embed.png",
-};
+    images: [
+      {
+        url: ogImage.src,
+        width: ogImage.width,
+        height: ogImage.height
+      },
+    ],
+  },
+  twitter: {
+    images: [
+      {
+        url: ogImage.src,
+        width: ogImage.width,
+        height: ogImage.height
+      },
+    ]
+  },
+}
 
 export default function RootLayout({ children }) {
   return (
