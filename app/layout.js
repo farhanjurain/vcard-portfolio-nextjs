2f9c7d1b54e2f65d136b40cfc151d452c1992224
soren <afrozaman12@gmail.com>
2024-02-01 18:27:19 +0600

    Final fix

--- app/layout.js
+++ app/layout.js
@@ -1,28 +1,25 @@
 import "./globals.css";
 import SideBar from "./layouts/SideBar";
 import NavBar from "./layouts/NavBar";
-import Head from "next/head";
-import favicon from "@/public/favicon.ico";
+import embed from "@/public/embed.png";
 
+export const metadata = {
+  title: "Soren D. Blank",
+  description: "Im gonna be the king of the pirates.",
+  image: embed,
+  keywords: ["Soren", "Blank", "Soren D. Blank", "Soren Blank"],
+  "og:title" : "Soren D. Blank",
+  "og:description" : "Im gonna be the king of the pirates.",
+  "og:image" : embed,
+
+  "twitter:title" : "Soren D. Blank",
+  "twitter:description" : "Im gonna be the king of the pirates.",
+  "twitter:image" : embed,
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <title>Soren D. Blank</title>
-        <meta name="description" content="I will be the king of the pirates." />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta property="og:title" content=""/>
-        <meta property="og:description" content="I will be the king of the pirates." />
-        <meta property="og:image" content=""/>
-        <meta property="og:url" content=""/>
-        <meta name="twitter:title" content=""/>
-        <meta name="twitter:description" content="I will be the king of the pirates." />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:image" content=""/>
-        <link rel="icon" href={favicon} />
-
-      </Head>
       <body>
         <main>
           <SideBar />
