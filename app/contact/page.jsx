95c2493320bf9282f6a128209d682be57e140c5d
Farhan Ahmed Jurain <farhanahmedjurain@gmail.com>
2025-05-29 00:09:46 +0600

    Update page.jsx

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -53,14 +53,6 @@ export default function Contact() {
                 <h2 className="h2 article-title">Contact</h2>
             </header>
 
-            <section className="mapbox" data-mapbox>
-                <figure>
-                    <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d233667.499305022!2d90.25487485406434!3d23.781067237446248!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b8b087026b81%3A0x8fa563bbdd5904c2!2sDhaka!5e0!3m2!1sen!2sbd!4v1706775356012!5m2!1sen!2sbd"
-                        width="400" height="300" loading="lazy"></iframe>
-                </figure>
-            </section>
-
             <section className="contact-form">
                 <h3 className="h3 form-title">Contact Form</h3>
                 <form ref={formRef} onSubmit={handleSubmit} action="#" className="form" data-form>
